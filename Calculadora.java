a848c727fa436ce981fab1ee3dfe2dd5f919fd28
Alex Dezso <alexandrodezso.alu@iespacomolla.es>
2024-11-28 19:29:07 +0100

    Modificación realizada

---
+++ Calculadora.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Introduzca num:");
+        int num1 = input.nextInt();
+        
+        System.out.print("Introduzca la operación: ");
+        String operacion = input.next();
+        
+        System.out.print("Introduzca num:");
+        int num2 = input.nextInt();
+        
+        switch (operacion) {
+            case "+":
+                int suma = num1 + num2;
+                
+                System.out.printf("Resultado de %d + %d = %d\n", num1, num2, suma);
+                break;
+            case "-":
+                int resta = num1 - num2;
+                System.out.printf("Resultado de %d - %d = %d\n", num1, num2, resta);
+                break;
+            
+            case "*":
+                int multiplicacion = num1 * num2;
+                System.out.printf("Resultado de %d * %d = %d\n", num1, num2, multiplicacion);
+                break;
+                
+            case "/":
+                int division = num1 / num2;
+                int resto = num1 % num2;
+                System.out.printf("Resultado de %d / %d = %d con un resto = %d\n", num1, num2, division, resto);
+                break;          
+        }
+    }
+    
+}
